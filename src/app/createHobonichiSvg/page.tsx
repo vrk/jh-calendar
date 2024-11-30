a6bd6b4b01fa93afef5e1c5772d9d8ef51e75bd8
vrk <victoriakirst@gmail.com>
2024-11-30 18:17:00 -0500

    add rows

--- src/app/createHobonichiSvg/page.tsx
+++ src/app/createHobonichiSvg/page.tsx
@@ -42,14 +42,61 @@ function createSvgElement(name: string) {
 
 function createCousinSvg() {
   const svg = createSvgElement("svg");
-  svg.setAttribute('style', 'border: 1px solid black');
-  svg.setAttribute("width", `${GRID_BOX_WIDTH_IN_PIXELS * NUMBER_BOXES_PER_PAGE_WIDTH * 2}`);
-  svg.setAttribute("height", `${GRID_BOX_WIDTH_IN_PIXELS * NUMBER_BOXES_PER_PAGE_HEIGHT}`);
+  svg.setAttribute("style", "border: 1px solid black");
+  svg.setAttribute(
+    "width",
+    `${GRID_BOX_WIDTH_IN_PIXELS * NUMBER_BOXES_PER_PAGE_WIDTH * 2}`
+  );
+  svg.setAttribute(
+    "height",
+    `${GRID_BOX_WIDTH_IN_PIXELS * NUMBER_BOXES_PER_PAGE_HEIGHT}`
+  );
   svg.setAttributeNS(
     "http://www.w3.org/2000/xmlns/",
     "xmlns:xlink",
     "http://www.w3.org/1999/xlink"
   );
 
+  const leftStartingX = NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS;
+  const leftStartingY = NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS;
+  const leftPageGrid = createGridsForPage(leftStartingX, leftStartingY);
+
+  const rightStartingX =
+    NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS +
+    NUMBER_BOXES_PER_PAGE_WIDTH * GRID_BOX_WIDTH_IN_PIXELS;
+  const rightStartingY = NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS;
+  const rightPageGrid = createGridsForPage(rightStartingX, rightStartingY);
+  svg.append(leftPageGrid);
+  svg.append(rightPageGrid);
+
   return svg;
+}
+
+function createGridsForPage(startingX: number, startingY: number) {
+  // <line x1="0" y1="3" x2="30" y2="3" stroke-dasharray="4" />
+  const group = createSvgElement("g");
+  for (let i = 0; i < NUMBER_BOXES_PER_PAGE_HEIGHT - 2 * NUMBER_BOXES_IN_MARGIN; i++) {
+    const x1 = startingX;
+    const y1 = startingY + i * GRID_BOX_WIDTH_IN_PIXELS;
+    const x2 =
+      startingX +
+      NUMBER_BOXES_PER_PAGE_WIDTH * GRID_BOX_WIDTH_IN_PIXELS -
+      2 * NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS;
+    const y2 = y1;
+    const line = createLine(x1, y1, x2, y2);
+    group.append(line);
+  }
+  return group;
+}
+
+function createLine(x1: number, y1: number, x2: number, y2: number) {
+  const line = createSvgElement("line");
+  line.setAttribute("x1", `${x1}`);
+  line.setAttribute("y1", `${y1}`);
+  line.setAttribute("x2", `${x2}`);
+  line.setAttribute("y2", `${y2}`);
+  line.setAttribute("stroke-dasharray", "4");
+  line.setAttribute("stroke-dasharray", "4");
+  line.setAttribute("stroke", "black");
+  return line;
 }

02eb0d0e1d66c139cbfdaba9caa76d6144ee6037
vrk <victoriakirst@gmail.com>
2024-12-01 07:58:52 -0500

    date headers

--- src/app/createHobonichiSvg/page.tsx
+++ src/app/createHobonichiSvg/page.tsx
@@ -15,8 +15,8 @@ const NUMBER_OF_COLUMNS = 4;
 const NUMBER_OF_ROWS = 5;
 
 const NUMBER_PIXELS_PER_DAY = NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS;
-const NUMBER_PIXELS_PER_MARGIN = NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS;
-
+const NUMBER_PIXELS_PER_MARGIN =
+  NUMBER_BOXES_IN_MARGIN * GRID_BOX_WIDTH_IN_PIXELS;
 
 export default function HobonichiSvg() {
   const overallContainer = React.useRef<HTMLDivElement>(null);
@@ -88,7 +88,11 @@ function createCousinSvg() {
 function createGridsForPage(startingX: number, startingY: number) {
   // <line x1="0" y1="3" x2="30" y2="3" stroke-dasharray="4" />
   const group = createSvgElement("g");
-  for (let y = 0; y <= NUMBER_BOXES_PER_PAGE_HEIGHT - 2 * NUMBER_BOXES_IN_MARGIN; y++) {
+  for (
+    let y = 0;
+    y <= NUMBER_BOXES_PER_PAGE_HEIGHT - 2 * NUMBER_BOXES_IN_MARGIN;
+    y++
+  ) {
     const x1 = startingX;
     const y1 = startingY + y * GRID_BOX_WIDTH_IN_PIXELS;
     const x2 =
@@ -99,7 +103,11 @@ function createGridsForPage(startingX: number, startingY: number) {
     const line = createLine(x1, y1, x2, y2);
     group.append(line);
   }
-  for (let x = 0; x <= NUMBER_BOXES_PER_PAGE_WIDTH - 2 * NUMBER_BOXES_IN_MARGIN; x++) {
+  for (
+    let x = 0;
+    x <= NUMBER_BOXES_PER_PAGE_WIDTH - 2 * NUMBER_BOXES_IN_MARGIN;
+    x++
+  ) {
     const x1 = startingX + x * GRID_BOX_WIDTH_IN_PIXELS;
     const y1 = startingY;
     const x2 = x1;
@@ -113,39 +121,59 @@ function createGridsForPage(startingX: number, startingY: number) {
   return group;
 }
 
-function createDates(startingX: number, startingY: number, skipFirstColumn: boolean) {
+function createDates(
+  startingX: number,
+  startingY: number,
+  skipFirstColumn: boolean
+) {
+  const labels = skipFirstColumn
+    ? ["", "MON", "TUE", "WED"]
+    : ["THU", "FRI", "SAT", "SUN"];
   const group = createSvgElement("g");
   for (let row = 0; row < NUMBER_OF_ROWS; row++) {
     for (let col = 0; col < NUMBER_OF_COLUMNS; col++) {
       if (col === 0 && skipFirstColumn) {
         continue;
       }
-      
-      const x = startingX + col * NUMBER_PIXELS_PER_DAY; 
-      const y = startingY + row * NUMBER_PIXELS_PER_DAY + NUMBER_PIXELS_PER_MARGIN; 
+
+      const x = startingX + col * NUMBER_PIXELS_PER_DAY;
+      const y =
+        startingY + row * NUMBER_PIXELS_PER_DAY + NUMBER_PIXELS_PER_MARGIN;
       const dateSquare = createDateSquare(x, y);
       group.append(dateSquare);
     }
   }
   for (let col = 0; col < NUMBER_OF_COLUMNS; col++) {
-    const x = startingX + col * NUMBER_PIXELS_PER_DAY; 
+    const x = startingX + col * NUMBER_PIXELS_PER_DAY;
     const y = startingY;
     const isDouble = skipFirstColumn && col === 0;
-    const square = createHeaderFill(x, y, isDouble);
-    group.append(square);
+    const label = labels[col];
+    if (isDouble) {
+      const square = createHeaderFill(x, y, isDouble);
+      group.append(square);
+    } else {
+      const square = createDayHeader(x, y, label);
+      group.append(square);
+    }
   }
   return group;
 }
 
 function createDateSquare(startingX: number, startingY: number) {
   const group = createSvgElement("g");
-  const squareFill = createDateHeaderFill(startingX, startingY);
+  const squareFill = createDaySubheadFill(startingX, startingY);
   group.append(squareFill);
   const square = createSvgElement("rect");
-  square.setAttribute("width", `${NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS}`);
-  square.setAttribute("height", `${NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS}`);
-  square.setAttribute("x", `${startingX}`)
-  square.setAttribute("y", `${startingY}`)
+  square.setAttribute(
+    "width",
+    `${NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS}`
+  );
+  square.setAttribute(
+    "height",
+    `${NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS}`
+  );
+  square.setAttribute("x", `${startingX}`);
+  square.setAttribute("y", `${startingY}`);
   square.setAttribute("stroke", "black");
   square.setAttribute("shape-rendering", "crispEdges");
   square.setAttribute("fill", "transparent");
@@ -165,38 +193,95 @@ function createLine(x1: number, y1: number, x2: number, y2: number) {
   return line;
 }
 
-function createHeaderFill(startingX: number, startingY: number, isDouble: boolean) {
-  return createHeaderFillInside(startingX, startingY, isDouble, true);
+function createDayHeader(startingX: number, startingY: number, message: string) {
+  const fillGroup = createHeaderFill(startingX, startingY, false);
+  const text = createText(message, startingX, startingY, 2, Position.MiddleRight);
+  fillGroup.append(text);
+  return fillGroup;
 }
 
-function createDateHeaderFill(startingX: number, startingY: number) {
-  return createHeaderFillInside(startingX, startingY, false, false);
+function createHeaderFill(
+  startingX: number,
+  startingY: number,
+  isDouble: boolean
+) {
+  return createFillSvg(startingX, startingY, isDouble, true);
 }
 
-function createHeaderFillInside(startingX: number, startingY: number, isDouble: boolean, hasBorder: boolean) {
+function createDaySubheadFill(startingX: number, startingY: number) {
+  return createFillSvg(startingX, startingY, false, false);
+}
+
+function createFillSvg(
+  startingX: number,
+  startingY: number,
+  isDouble: boolean,
+  hasBorder: boolean
+) {
   const group = createSvgElement("g");
   const square = createSvgElement("rect");
-  const numberBoxesTall = isDouble ? 2 * NUMBER_BOXES_IN_HEADER : NUMBER_BOXES_IN_HEADER; 
-  square.setAttribute("width", `${NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS}`);
-  square.setAttribute("height", `${numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS}`);
-  square.setAttribute("x", `${startingX}`)
-  square.setAttribute("y", `${startingY}`)
+  const numberBoxesTall = isDouble
+    ? 2 * NUMBER_BOXES_IN_HEADER
+    : NUMBER_BOXES_IN_HEADER;
+  square.setAttribute(
+    "width",
+    `${NUMBER_BOXES_PER_DAY * GRID_BOX_WIDTH_IN_PIXELS}`
+  );
+  square.setAttribute(
+    "height",
+    `${numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS}`
+  );
+  square.setAttribute("x", `${startingX}`);
+  square.setAttribute("y", `${startingY}`);
   if (hasBorder) {
     square.setAttribute("stroke", "black");
   }
   square.setAttribute("shape-rendering", "crispEdges");
   square.setAttribute("fill", isDouble ? "gray" : "white");
-  //   <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">TEXT</text>    
-  const text = createSvgElement("text");
-  const offset = GRID_BOX_WIDTH_IN_PIXELS;
-  text.setAttribute("x", `${startingX + offset}`)
-  text.setAttribute("y", `${startingY + numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS / 2 + 1}`)
-  text.setAttribute("height", `${numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS}`)
-  text.setAttribute("dominant-baseline", 'middle');
-  text.setAttribute("text-anchor", 'middle');
-  text.setAttribute("style", 'color: black');
-  text.innerHTML = "25";
+  //   <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">TEXT</text>
+  // const text = createSvgElement("text");
+  // const offset = GRID_BOX_WIDTH_IN_PIXELS;
+  // text.setAttribute("x", `${startingX + offset}`);
+  // text.setAttribute(
+  //   "y",
+  //   `${startingY + (numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS) / 2 + 1}`
+  // );
+  // text.setAttribute("height", `${numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS}`);
+  // text.setAttribute("dominant-baseline", "middle");
+  // text.setAttribute("text-anchor", "middle");
+  // text.setAttribute("style", "color: black");
+  // text.innerHTML = "25";
   group.append(square);
-  group.append(text);
+  // group.append(text);
   return group;
+}
+
+enum Position {
+  Left,
+  MiddleRight,
+}
+
+function createText(
+  message: string,
+  startingX: number,
+  startingY: number,
+  numberBoxesTall: number,
+  position: Position
+) {
+  const text = createSvgElement("text");
+  let offset = GRID_BOX_WIDTH_IN_PIXELS;
+  if (position === Position.MiddleRight) {
+    offset = GRID_BOX_WIDTH_IN_PIXELS * 5;
+  }
+  text.setAttribute("x", `${startingX + offset}`);
+  text.setAttribute(
+    "y",
+    `${startingY + (numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS) / 2 + 1}`
+  );
+  text.setAttribute("height", `${numberBoxesTall * GRID_BOX_WIDTH_IN_PIXELS}`);
+  text.setAttribute("dominant-baseline", "middle");
+  text.setAttribute("text-anchor", "middle");
+  text.setAttribute("style", "color: black");
+  text.innerHTML = message;
+  return text;
 }

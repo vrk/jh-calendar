9f6ce181aa2f40b2e9677366064634724153a7cb
vrk <victoriakirst@gmail.com>
2024-12-01 12:14:54 -0500

    basic style

--- src/components/CalendarView/CalendarView.tsx
+++ src/components/CalendarView/CalendarView.tsx
@@ -1,8 +1,30 @@
-import * as React from 'react';
-import style from './CalendarView.module.css';
+import * as React from "react";
+import style from "./CalendarView.module.css";
+import { CalendarContext } from "../CalendarContextProvider/CalendarContextProvider";
+import { createCousinCalendarSvg } from "@/helpers/hobonichi-generator";
+import { getYearMonthString } from "@/helpers/calendar-helpers";
 
 function CalendarView() {
-  return <div className={style.container}></div>;
+  const { yearMonthInfo, calendarFunctions } =
+    React.useContext(CalendarContext);
+  const overallContainer = React.useRef<HTMLDivElement>(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!overallContainer.current || isLoaded) {
+      return;
+    }
+    const box = createCousinCalendarSvg(getYearMonthString(yearMonthInfo));
+    overallContainer.current.append(box);
+    setIsLoaded(true);
+    return () => {
+      if (!overallContainer.current) {
+        return;
+      }
+      overallContainer.current.innerHTML = "";
+    };
+  }, [overallContainer]);
+  return <div className={style.container} ref={overallContainer}></div>;
 }
 
 export default CalendarView;
